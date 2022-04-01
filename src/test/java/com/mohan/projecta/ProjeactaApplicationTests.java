0f29dcec0923a930005b4f520e21c28df25a57db
Manchashetty <hz319354@IL-3C6MD6R.local>
2022-04-01 16:57:05 -0500

    first commit

---
+++ src/test/java/com/mohan/projecta/ProjeactaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan.projecta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjeactaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
